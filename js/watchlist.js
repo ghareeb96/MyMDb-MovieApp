571de949ce682790b527d180806357a5204ff2c6
unknown <ghareeb_shehata@outlook.com>
2020-08-10 14:30:39 +0200

    Watched & Watchlist pages are done in UI and Functionaities

--- js/watchlist.js
+++ js/watchlist.js
@@ -1,28 +1,11 @@
-let url = "https://www.omdbapi.com/?apikey=7d1c1e6c";
-let page;
-let movieID;
+$(document).ready(() => {
+    let watchedList = JSON.parse(localStorage.getItem("watchlist"));
 
-
-const noPic = "https://upload.wikimedia.org/wikipedia/en/6/60/No_Picture.jpg";
-$("#search-btn").click(() => {
-        page = 1;
-        newUrl = url + "&s=" + $("#search-input").val() + "&page=" + page;
-        if ($(".searchOptions").hasClass("optionsActive")) {
-            if ($("#type").val() != "any") {
-                newUrl += "&type=" + $("#type").val();
-            }
-            if ($("#year").val() != "") {
-                newUrl += "&y=" + $("#year").val();
-            }
-        }
-
-        $.get(newUrl).then(response => {
-            $(".moviesContainer").html("");
-            let movies = response.Search;
-            $.each(movies, (index, movie) => {
-
-                $(".moviesContainer").append(
-                    `
+    watchedList.forEach((movie_id) => {
+        $.get(`https://www.omdbapi.com/?apikey=7d1c1e6c&i=${movie_id}`).then(response => {
+            let movie = response;
+            $(".moviesContainer").append(
+                `
                 <div class="movieContainer">
                     <div class="movie-img">
                         <img src="${movie.Poster == 'N/A' ?   noPic : movie.Poster }" alt="Poster">
@@ -35,65 +18,13 @@ $("#search-btn").click(() => {
                     </div>
                 </div>
                 `
-                );
-            })
+            );
         })
-
-
-        page++;
-
-
-
-    }
-
-)
-
-
-
-$(window).on('scroll', function () {
-    var scrollPosition = $(window).scrollTop() + $(window).outerHeight();
-    var divTotalHeight = $(".moviesContainer")[0].scrollHeight + $(".moviesContainer")[0].offsetTop + parseInt($(".moviesContainer").css("margin-bottom"));
-
-    if (scrollPosition >= divTotalHeight) {
-        newUrl = url + "&s=" + $("#search-input").val() + "&page=" + page;
-        if ($(".searchOptions").hasClass("optionsActive")) {
-            if ($("#type").val() != "any") {
-                newUrl += "&type=" + $("#type").val();
-            }
-            if ($("#year").val() != "" && $("#year").val() >= 1950 && $("#year").val() <= 2020) {
-                newUrl += "&y=" + $("#year").val();
-            }
-        }
-        $.get(newUrl).then(response => {
-            let movies = response.Search;
-            $.each(movies, (index, movie) => {
-                $(".moviesContainer").append(
-                    `
-                <div class="movieContainer">
-                    <div class="movie-img">
-                        <img  src="${movie.Poster == 'N/A' ?   noPic : movie.Poster }" alt="Poster">
-                    </div>
-                    <div class="movie-info">
-                        <h5 class="title">${movie.Title}</h5>
-                        <h6 class="year">${movie.Year}</h6>
-                        <h6 class="type">${movie.Type}</h6>
-                        <button id="${movie.imdbID}" class="movie-details">More details</button>
-                    </div>
-                </div>
-                `
-                );
-            })
-        })
-        page++;
-    }
-});
-
-$("#searchOpBtn").on("click", () => {
-    $(".searchOptions").toggleClass("optionsActive");
+    })
 })
 
-$(document).on("click", ".movie-details", function (e) {
 
+$(document).on("click", ".movie-details", function (e) {
 
     $.get(`https://www.omdbapi.com/?apikey=7d1c1e6c&i=${e.target.id}&plot=full`).then(response => {
         movieID = e.target.id;
@@ -173,7 +104,6 @@ $(document).on("click", ".movie-details", function (e) {
         $(".my-modal").addClass("modal-open");
     })
 })
-
 $(document).on("click", ".my-modal", (e) => {
     if (e.target.classList.contains("my-modal")) {
         $(".my-modal").removeClass("modal-open");
